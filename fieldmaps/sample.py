ff23d5a4062ee2aabeb22e8d823443941f23a5fa
Zenna Tavares <zennatavares@gmail.com>
2017-07-13 18:36:57 -0400

    sample runs and makes images, sanity check works

--- fieldmaps/sample.py
+++ fieldmaps/sample.py
@@ -4,6 +4,11 @@ import rbf
 import matplotlib.pyplot as plt
 import torch
 from torch.autograd import Variable
+import torch.optim as optim
+import numpy as np
+
+plt.ion()
+
 
 def mnist_batch(batch_size=1):
     train_loader = torch.utils.data.DataLoader(
@@ -19,9 +24,16 @@ def plot_query_points(query_points):
     plt.scatter(query_points[:,0], query_points[:,1])
 
 
-def main():
+def main(niters=100000):
     query_points = torch.rand(28 * 28, 2) * 28
     grid_points = rbf.tensor_to_field_coords(28, 28)
+    grid_points = Variable(grid_points)
+    query_points = Variable(query_points, requires_grad=True)
+    optimizer = optim.SGD([query_points], lr=1, momentum=0.1)
+
+    # Minimize
+    dist = query_points - grid_points
+    loss = torch.mean(dist * dist)
     # query_points = query_points.numpy()
     # plot_query_points(query_points)
 
@@ -30,12 +42,27 @@ def main():
         # import pdb; pdb.set_trace()
         img = data[0, 0]
         weights = rbf.mono_img_batch_to_weights(data)
-        transformed = rbf.torch_rbf(query_points.unsqueeze(0), grid_points.unsqueeze(0), weights)
 
-        # plt.imshow(img.numpy())
-        plt.figure()
-        plt.imshow(transformed.numpy().reshape(28, 28))
-        plt.show()
+        myobj = plt.imshow(np.random.rand(28, 28))
+        for i in range(niters):
+            transformed = rbf.torch_rbf(query_points.data.unsqueeze(0), grid_points.data.unsqueeze(0), weights)
+            # plt.imshow(img.numpy())
+            # plt.figure()
+            img_data = transformed.numpy().reshape(28, 28)
+            # img_data = np.random.rand(28, 28)
+            if i % 100 == 0:
+                myobj.set_data(img_data)
+                plt.draw()
+                plt.pause(0.001)
+            # plt.imshow(transformed.numpy().reshape(28, 28))
+            # plt.show()
+            optimizer.zero_grad()
+            dist = query_points - grid_points
+            loss = torch.mean(dist * dist)
+            print("Lossy", loss)
+            loss.backward()
+            optimizer.step()
+
     # plot the query points
 
 main()
